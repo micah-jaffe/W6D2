47178bdc2c59dcac8f63940418513f798f3d2030
micah-jaffe <micah.jaffe95@gmail.com>
2018-11-27 12:21:44 -0800

    Complete View#setupBoard method

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,6 +1,11 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');
+const Game = require('../backend/game.js');
 
   $(() => {
-    // Your code here
+    const game = new Game();
+    const view = new View(game, $('.ttt'));
+    console.log(game);
+    console.log(view);
+    view.setupBoard();
+
   });
--- tictactoe/src/ttt-view.js
+++ tictactoe/src/ttt-view.js
@@ -1,11 +1,29 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    let $li = $('li');
+    let pos = $li.data('position');
+  }
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let $ul = $('<ul></ul>');
+    this.$el.append($ul);
+
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        let $li = $('<li></li>');
+        $ul.append($li);
+
+        $li.attr('position', [i, j]);
+      }
+    }
+  }
 }
 
 module.exports = View;
